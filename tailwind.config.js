72712d70c11ec3c6f2b34b6f9ff2cda20d83f907
nafistanjim <99764910+nafistanjim@users.noreply.github.com>
2024-10-02 22:21:47 +0600

    Final Uploading All Files
    Uploading all files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Enable dark mode by adding the 'dark' class
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
